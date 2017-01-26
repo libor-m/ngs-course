6c11cc646ae3d825e02b6731b7b55bc96f5ede3c
Libor Morkovsky <morkovsk@natur.cuni.cz>
2017-01-27 00:29:10 +0100

    convert to unix newlines

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'UNIX and NGS'
-copyright = u'2015, Libor Morkovsky, Vaclav Janousek'
+copyright = u'Libor Morkovsky, Vaclav Janousek'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
